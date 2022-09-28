a7cebc5838c1c1fdc2a9c20c5e950457bad06265
KIMSEONGRAN <doll950904@gmail.com>
2022-09-28 17:04:29 +0900

    :sparkles: Merge Nodes ... Zeros 풀이

--- algorithm/Merge Nodes in Between Zeros/solution.js
+++ algorithm/Merge Nodes in Between Zeros/solution.js
@@ -15,10 +15,12 @@ var mergeNodes = function (head) {
   let value = head;
 
   while (true) {
+    // 다음 노드가 없으면 반복문 종료
     if (value.next === null) {
       break;
     }
 
+    // 0을 만나면 변수 초기화 및 배열에 값 담기
     if (value.next.val === 0) {
       arr.push(sum);
       sum = 0;
@@ -28,10 +30,12 @@ var mergeNodes = function (head) {
       value = value.next;
     }
   }
+
   let i = 0;
   let answer = new ListNode(arr[i]);
   let result = answer;
 
+  // 배열에 담은 sum값을 노드로 출력
   while (true) {
     if (arr.length - 1 === i) {
       return result;
